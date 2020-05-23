ee75785f3d727a85a673bcd67dbef88e60af99f1
Matthew Elwin <elwin@northwestern.edu>
2020-05-23 14:06:30 -0500

    Enable IOMMU.

--- install_bootloader.sh
+++ install_bootloader.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Command used to install the efistub boot entry
 efibootmgr -b 0000 -B 
-efibootmgr --disk /dev/nvme0n1 --part 1 --create --label 'Arch Linux' --loader /vmlinuz-linux --unicode 'root=PARTUUID=47ed303a-7e00-274f-89c2-951d25a98d4d rw initrd=\amd-ucode.img initrd=\initramfs-linux.img fbcon=rotate:3' --verbose
+efibootmgr --disk /dev/nvme0n1 --part 1 --create --label 'Arch Linux' --loader /vmlinuz-linux --unicode 'root=PARTUUID=47ed303a-7e00-274f-89c2-951d25a98d4d rw initrd=\amd-ucode.img initrd=\initramfs-linux.img fbcon=rotate:3 amd_iommu=on iommu=pt' --verbose
